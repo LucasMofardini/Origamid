7164e23847d6784b4a604017f8ecc940b868df65
LucasMofardini <lucasmofardini2@gmail.com>
2020-12-07 22:38:46 -0300

    [Js e JQuery] Aula 1, 2 e 3

---
+++ JavaScript e JQuery/script.js
@@ -0,0 +1,20 @@
+var nome = "lucas"
+var idade = 18;
+var x = 10;
+var y = 12;
+y = 17;
+nome = "Gabriel"
+var resultado  = x + y *(y * x);
+console.log(resultado);
+console.log(typeof resultado);
+
+var sobrenome = "Mofardini"
+var nomeCompleto = nome +" "+ sobrenome;
+var concat = nome + x + " LUCAS " + nomeCompleto + " 12 " + y;
+console.log(concat);
+console.log(nomeCompleto);
+
+var faculdade = true;
+if (faculdade){
+    console.log("faculdade é verdadeiro");
+}
