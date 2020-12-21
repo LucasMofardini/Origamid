4325d3e76fbae3a73fa290d02e55e8d00ec51f90
Lucas Mofardini <lucasmofardini@gmail.com>
2020-12-21 00:09:53 -0300

    [Script] até function

--- JavaScript e JQuery/script.js
+++ JavaScript e JQuery/script.js
@@ -45,3 +45,64 @@ videoGames.splice(1,1);
 
 //primeiro numero = ordem dos itens na array
 //segundo numero = quantidade depois do  item 
+
+var o = 20;
+if (o > 32){
+    console.log('Maior que 12');
+}else{
+    console.log('Não pode');
+}
+var faculdade = true;
+if(faculdade){
+   console.log('adiciona 100');
+   }else if(faculdade){
+   console.log('adiciona 200');
+   }
+
+if(10 > 5){
+    console.log('10 é maior');
+}
+if((15 > 5) && (20 < 10)){
+    console.log('10 e maior'    );
+    
+}else{
+    console.log('errado');
+}
+
+
+function areaQuadrado(lado){
+    var area = lado * lado;
+    return area;
+}
+console.log(areaQuadrado(4));
+
+function imc(peso, altura){
+    var imcTotal = peso / (altura * altura);
+    return imcTotal;
+}
+console.log("Pessoa 1" + imc(67,1.80));
+
+
+function corPreferida (cor){
+    if(cor == "azul"){
+        console.log("Voce gosta de azul");
+    }else if(cor == "amarelo"){
+        console.log("Voce gosta de amarelo");
+    } if(cor == "roxo"){
+        console.log("Voce gosta de roxo");
+    }else{
+        console.log("Voce nao gosta de nada")
+    }
+}
+corPreferida('roxo');
+
+var custoDoCarro = function(portas){
+    var precoInicial = 10000;
+    if(portas){
+        return portas * precoInicial;
+    }else{
+        return 2 * precoInicial;
+    }
+}
+console.log(custoDoCarro(4));
+console.log(custoDoCarro());
