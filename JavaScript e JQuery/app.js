7d2569e261adb64623d646eac0a0f98d43599744
Lucas Mofardini <lucasmofardini@gmail.com>
2020-12-27 03:06:34 -0300

    [JQuery] Eventos basicos

--- JavaScript e JQuery/app.js
+++ JavaScript e JQuery/app.js
@@ -16,7 +16,7 @@ $('p').append(' <span>HOLAA</span>');
 
 //Se voce passa sem nenhum parametro ele serve como GET do valor. Pega apenas o primeiro valor
 console.log($('h1').html());
-//Se voce passa com alguma coisa dentro ele subistitui
+//Se voce passa com   alguma coisa dentro ele subistitui
 $( "button" ).html( "Next Step..." );
 
 // APPEND ADICIONA CONTEUDO | HTML SUBISTITUI
@@ -40,3 +40,43 @@ $('p').before('<span> Antes da tag </tag>');
 $('p').wrap('<div class="div-teste"></div>');
 //Remove o elemento Pai do item selecionado
 $('p').unwrap();
+
+// EVENTOS BASICOS
+
+$('p').click(function(){
+  $('p').text('LUUCAS');
+});
+
+// ou
+$('li').on('click', function(){
+  $('button').text('DAORA');
+});
+// Esse preventDefault(); voce anula o que o objeto a iria fazer e deixa o que a funçao vai fazer
+// $('a').click(function(e){
+//   e.preventDefault();
+//   $(this).text("clicou");
+// });]
+
+//Um hover que nao muda, instantaneo
+$('li').on('mouseenter', function(){
+  $('button').text('DAORA');
+});
+//ex de tirar e colocar o hover
+
+// $('a').mouseenter(function(e){
+//   e.preventDefault();
+//   $(this).text("clicou");
+// });
+// $('a').on('mouseleave',function(e){
+//   e.preventDefault();
+//   $(this).text("Abrir modal");
+// });
+
+//O tanto que o scroll ta do topo
+//$(document).scrollTop();
+
+// mostra o tamanho da tela
+// $(window).on('resize', function(){
+//   $('a').text($('body').width());
+// });
+//
