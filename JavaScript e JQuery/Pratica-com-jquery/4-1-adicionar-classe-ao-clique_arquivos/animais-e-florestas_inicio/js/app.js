8b4445f5d7218b895c6f579613dda205c0f14ec2
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-12 00:01:37 -0300

    [JQuery] tab funcionando

--- JavaScript e JQuery/Pratica-com-jquery/4-1-adicionar-classe-ao-clique_arquivos/animais-e-florestas_inicio/js/app.js
+++ JavaScript e JQuery/Pratica-com-jquery/4-1-adicionar-classe-ao-clique_arquivos/animais-e-florestas_inicio/js/app.js
@@ -1,6 +1,9 @@
 $('.tab-menu a').on('click', function(e){
   e.preventDefault();
-  var itemID =   $(this).attr('href');
+  var itemID =  $(this).attr('href');
+    //remove
+    $('.tab-menu a, .item').removeClass('active');
+
     $(this).addClass('active');
     $(itemID).addClass('active');
 });
