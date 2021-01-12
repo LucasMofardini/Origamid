48bcfec00de26280e948329982b52d6b5789955e
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-12 00:19:10 -0300

    [JQuery] caso o usuario nao tenha js

--- JavaScript e JQuery/Pratica-com-jquery/4-1-adicionar-classe-ao-clique_arquivos/animais-e-florestas_inicio/js/app.js
+++ JavaScript e JQuery/Pratica-com-jquery/4-1-adicionar-classe-ao-clique_arquivos/animais-e-florestas_inicio/js/app.js
@@ -1,9 +1,27 @@
-$('.tab-menu a').on('click', function(e){
-  e.preventDefault();
-  var itemID =  $(this).attr('href');
-    //remove
-    $('.tab-menu a, .item').removeClass('active');
+$(document).ready(function(){
+    $('.animais .tab-menu a').first().addClass('active');
+    $('.animais .item').first().addClass('active');
 
-    $(this).addClass('active');
-    $(itemID).addClass('active');
+    $('.animais .tab-menu a').on('click', function(e){
+      e.preventDefault();
+      var itemID =  $(this).attr('href');
+
+        //remove
+        $('.animais  .tab-menu a, .item').removeClass('active');
+        $(this).addClass('active');
+        $(itemID).addClass('active');
+    });
+
+    $('.florestas .tab-menu a').first().addClass('active');
+    $('.florestas .item').first().addClass('active');
+
+    $('.florestas .tab-menu a').on('click', function(e){
+      e.preventDefault();
+      var itemID =  $(this).attr('href');
+
+        //remove
+        $('.florestas  .tab-menu a, .item').removeClass('active');
+        $(this).addClass('active');
+        $(itemID).addClass('active');
+    });
 });
