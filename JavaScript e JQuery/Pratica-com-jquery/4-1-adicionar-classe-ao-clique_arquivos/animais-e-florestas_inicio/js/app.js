3fe77b47fd65be47955d785db098f634a7d28682
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-12 23:19:34 -0300

    [JQuery] Otimizando

--- JavaScript e JQuery/Pratica-com-jquery/4-1-adicionar-classe-ao-clique_arquivos/animais-e-florestas_inicio/js/app.js
+++ JavaScript e JQuery/Pratica-com-jquery/4-1-adicionar-classe-ao-clique_arquivos/animais-e-florestas_inicio/js/app.js
@@ -1,27 +1,33 @@
-$(document).ready(function(){
-    $('.animais .tab-menu a').first().addClass('active');
-    $('.animais .item').first().addClass('active');
-
-    $('.animais .tab-menu a').on('click', function(e){
-      e.preventDefault();
-      var itemID =  $(this).attr('href');
-
-        //remove
-        $('.animais  .tab-menu a, .item').removeClass('active');
-        $(this).addClass('active');
-        $(itemID).addClass('active');
-    });
-
-    $('.florestas .tab-menu a').first().addClass('active');
-    $('.florestas .item').first().addClass('active');
-
-    $('.florestas .tab-menu a').on('click', function(e){
-      e.preventDefault();
-      var itemID =  $(this).attr('href');
-
-        //remove
-        $('.florestas  .tab-menu a, .item').removeClass('active');
-        $(this).addClass('active');
-        $(itemID).addClass('active');
-    });
+//Funciona porem não é otimizado
+// $(document).ready(function(){
+//     $('.animais .tab-menu a').first().addClass('active');
+//     $('.animais .item').first().addClass('active');
+//
+//     $('.animais .tab-menu a').on('click', function(e){
+//       e.preventDefault();
+//       var itemID =  $(this).attr('href');
+//
+//         //remove
+//         $('.animais  .tab-menu a, .item').removeClass('active');
+//         $(this).addClass('active');
+//         $(itemID).addClass('active');
+//     });
+//
+//     $('.florestas .tab-menu a').first().addClass('active');
+//     $('.florestas .item').first().addClass('active');
+//
+//     $('.florestas .tab-menu a').on('click', function(e){
+//       e.preventDefault();
+//       var itemID =  $(this).attr('href');
+//
+//         //remove
+//         $('.florestas  .tab-menu a, .item').removeClass('active');
+//         $(this).addClass('active');
+//         $(itemID).addClass('active');
+//     });
+// });
+$('[data-group]').each(function(){
+    var $allTarget = $(this).find('[data-target]'),
+    $allClick = $(this).find('[data-click]');
+    console.log($allTarget);
 });
