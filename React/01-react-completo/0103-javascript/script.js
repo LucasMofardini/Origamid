2a7a2e2b6e308ee3d4e47f78e4a63094adc22e2c
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-19 23:51:53 -0300

    [React] Desestruturação de objeto

--- React/01-react-completo/0103-javascript/script.js
+++ React/01-react-completo/0103-javascript/script.js
@@ -1,7 +1,31 @@
-const grupoA = 500;
-const grupoB = 200;
+//Tudo em javascript é OBJETO
+const menu = {
+  seletor: '.principal',
+};
+console.log(menu.seletor.toUpperCase());
 
-const active = false;
-const button = active && 'Botão está ativo';
+//Função normal
+function upperName(name) {
+  return name.toUpperCase();
+}
+console.log(upperName('Lucas'));
+//Criando função com const (expressão) Arrow function
+const lowername = (name) => {
+  return name.toLowerCase();
+};
+console.log(lowername('LUCAS'));
 
-console.log(button);
+//Desestruturação de um objeto
+
+function handleMouse(event) {
+  const { clientX, clientY } = event;
+  console.log(clientX, clientY);
+}
+document.addEventListener('click', handleMouse);
+//Desestrutura na Orderm do array
+const frutas = ['banana', 'uva'];
+const [fruta1, fruta2] = frutas;
+console.log(fruta1);
+//banana
+console.log(fruta2);
+//uva
