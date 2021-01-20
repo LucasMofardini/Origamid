6dece8c97e11610df6b7a28018baa30e1a26a60d
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-20 00:15:53 -0300

    [React] Rest and Spread

--- React/01-react-completo/0103-javascript/script.js
+++ React/01-react-completo/0103-javascript/script.js
@@ -16,7 +16,6 @@ const lowername = (name) => {
 console.log(lowername('LUCAS'));
 
 //Desestruturação de um objeto
-
 function handleMouse(event) {
   const { clientX, clientY } = event;
   console.log(clientX, clientY);
@@ -38,3 +37,33 @@ const useQuadrado = [
 const [lados, perimetro] = useQuadrado;
 console.log(lados);
 console.log(perimetro(10));
+
+//Rest
+// os 3 pontinhos antes do parametro pega todo os valores passados e transformam em array
+function showList(empresa, ...clientes) {
+  clientes.forEach((cliente) => {
+    console.log(cliente, empresa);
+  });
+}
+showList('Google', 'lucas', 'Gabriel', 'Mofardini');
+
+//Spread
+//Espahar os itens da Array
+const numeros = [10, 2, 40];
+const maiorNumero = Math.max(...numeros);
+console.log(maiorNumero);
+
+const maior = Math.max(10, 5, 20);
+console.log(maior);
+
+const numero2 = [122, 32, ...numeros, 25, 123, 30];
+console.log(numero2);
+
+const carro = {
+  cor: 'azul',
+  portas: 4,
+};
+//Quando voce quer adicionar mas sem colocar no objeto inicial
+const carroAno = { ...carro, ano: 2002 };
+console.log(carroAno);
+console.log(carro);
