b11b3ea9995db6890ed27cdc8efe2c93c0387ad2
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-25 01:44:31 -0300

    [JS] Variaveis

--- JavaScript e JQuery/script.js
+++ JavaScript e JQuery/script.js
@@ -4,18 +4,18 @@ var x = 10;
 var y = 12;
 y = 17;
 nome = "Gabriel";
-var resultado  = x + y *(y * x);
+var resultado = x + y * (y * x);
 console.log(resultado);
 console.log(typeof resultado);
 
 var sobrenome = "Mofardini"
-var nomeCompleto = nome +" "+ sobrenome;
+var nomeCompleto = nome + " " + sobrenome;
 var concat = nome + x + " LUCAS " + nomeCompleto + " 12 " + y;
 console.log(concat);
 console.log(nomeCompleto);
 
 var faculdade = true;
-if (faculdade){
+if (faculdade) {
     console.log("faculdade é verdadeiro");
 }
 var x = 30;
@@ -27,7 +27,7 @@ console.log(x);
 
 
 
-var videoGames = ['PS5','XBOX','PS4'];
+var videoGames = ['PS5', 'XBOX', 'PS4'];
 console.log(videoGames[0]);
 
 var tamanhoVideoGames = videoGames.length;
@@ -40,110 +40,112 @@ var buscarItem = videoGames.indexOf('2DS');
 
 console.log(buscarItem);
 
-var removeItem = 
-videoGames.splice(1,1);
+var removeItem =
+    videoGames.splice(1, 1);
 
 //primeiro numero = ordem dos itens na array
 //segundo numero = quantidade depois do  item 
 
 var o = 20;
-if (o > 32){
+if (o > 32) {
     console.log('Maior que 12');
-}else{
+} else {
     console.log('Não pode');
 }
 var faculdade = true;
-if(faculdade){
-   console.log('adiciona 100');
-   }else if(faculdade){
-   console.log('adiciona 200');
-   }
+if (faculdade) {
+    console.log('adiciona 100');
+} else if (faculdade) {
+    console.log('adiciona 200');
+}
 
-if(10 > 5){
+if (10 > 5) {
     console.log('10 é maior');
 }
-if((15 > 5) && (20 < 10)){
-    console.log('10 e maior'    );
-    
-}else{
+if ((15 > 5) && (20 < 10)) {
+    console.log('10 e maior');
+
+} else {
     console.log('errado');
 }
 
 
-function areaQuadrado(lado){
+function areaQuadrado(lado) {
     var area = lado * lado;
     return area;
 }
 console.log(areaQuadrado(4));
 
-function imc(peso, altura){
+function imc(peso, altura) {
     var imcTotal = peso / (altura * altura);
     return imcTotal;
 }
-console.log("Pessoa 1" + imc(67,1.80));
+console.log("Pessoa 1" + imc(67, 1.80));
 
 
-function corPreferida (cor){
-    if(cor == "azul"){
+function corPreferida(cor) {
+    if (cor == "azul") {
         console.log("Voce gosta de azul");
-    }else if(cor == "amarelo"){
+    } else if (cor == "amarelo") {
         console.log("Voce gosta de amarelo");
-    } if(cor == "roxo"){
+    }
+    if (cor == "roxo") {
         console.log("Voce gosta de roxo");
-    }else{
+    } else {
         console.log("Voce nao gosta de nada")
     }
 }
 corPreferida('roxo');
 
-var custoDoCarro = function(portas){
+var custoDoCarro = function(portas) {
     var precoInicial = 10000;
-    if(portas){
+    if (portas) {
         return portas * precoInicial;
-    }else{
+    } else {
         return 2 * precoInicial;
     }
 }
 console.log(custoDoCarro(4));
 console.log(custoDoCarro());
 
-var clientes = ['Andre', 'Rafael', 'Lucas','José', 'Joao', 'Marcos'];
+var clientes = ['Andre', 'Rafael', 'Lucas', 'José', 'Joao', 'Marcos'];
 var totalArray = clientes.length;
-for(var i = 0; i < totalArray; i++){
+for (var i = 0; i < totalArray; i++) {
     console.log(clientes[i]);
-    if(clientes[i] == 'Lucas'){    
+    if (clientes[i] == 'Lucas') {
         console.log('Chegou no Lucas');
     }
 }
 
-var o =  0;
-while(o<10){
+var o = 0;
+while (o < 10) {
     console.log(o);
-      o++;
-      }
+    o++;
+}
 
 
-var numero = 0, maximo = 20;
+var numero = 0,
+    maximo = 20;
 
-for(;numero < maximo;){
-    
+for (; numero < maximo;) {
+
     console.log(numero);
-    numero+= 5;
+    numero += 5;
 }
 
 var lucas = {
-    nome : "Lucas",
+    nome: "Lucas",
     profissao: "Desenvolvedor",
-    idade: 18    
+    idade: 18
 }
 console.log(lucas.nome);
 
 var quadrado = {
     totalLados: 4,
-    area: function(lado){
+    area: function(lado) {
         return lado * lado;
     },
-    perimetro: function(){
+    perimetro: function() {
         return lado * this.totalLados;
     }
 }
@@ -154,9 +156,9 @@ var pi = Math.PI;
 console.log(pi);
 
 var pessoa = {
-    nome : 'Lucas',
+    nome: 'Lucas',
     idade: '18',
-    agradecer: function(){
+    agradecer: function() {
         return "obrigado " + this.nome;
     }
 }
@@ -165,11 +167,11 @@ console.log(typeof pessoa);
 var carro = {
     modelo: 'Audi',
     portas: 4,
-    
+
 }
 carro.portas = 20;
 carro.cor = "Branco"
-carro.mala = function(){
+carro.mala = function() {
     return "abrir mala";
 }
 var portas = carro.portas;
---
+++ JavaScript/Conceitos/Variaveis/script.js
@@ -0,0 +1,53 @@
+//Responsavel por guardar algo na memoria
+var nome = 'lucas';
+let idade = 18;
+const possuiFaculdade = false;
+console.log(nome, idade, possuiFaculdade);
+
+//Usada para evitar repetição
+
+var preco = 10;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome = 'Mofardini',
+    cidade = 'SP',
+    bairro = 'Capão';
+console.log(sobrenome, cidade, bairro);
+
+//js é CASE SENSITIVE
+//Tem algumas palavras reservadas = Var, Break ...
+
+//Sempre joga a variavel sem valor pra cima e atribui valor nela depois. 
+console.log('Sem atribuir : ' +
+    comida);
+var comida = 'Calabresa';
+console.log('Atribuindo : ' +
+    comida);
+
+//Mudar valor atribuido
+var time = "Vasco";
+time = "Palmeiras";
+console.log('Time : ' + time);
+let pizza = 'Calabresa';
+pizza = 'Toscana';
+console.log('Pizza : ', pizza);
+//Const nunca muda
+
+
+// -- Exercicio 
+// 1 - Declarar uma variável com o seu nome
+var nomeCompleto = 'Lucas Gabriel Mofardini de Almeida';
+// 2 - Declarar uma variável com a sua idade
+var idadePessoa = 18;
+// 3 - Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFav;
+// 4 - Atribuir valor a sua comida favorita
+comidaFav = 'Pizza';
+// 5 - Declarar 5 variáveis diferentes sem valores
+var num1, num2, num3, num4, num5;
+
+console.log('Nome Completo : ' +
+    nome, 'Idade : ' + idade, 'Comida Favorita : ' + comidaFav);
