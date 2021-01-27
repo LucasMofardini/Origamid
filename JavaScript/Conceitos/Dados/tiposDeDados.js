d5b069556c024e56096bc3818aff1ce4449f55ef
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-26 23:40:25 -0300

    [JS] Type of data - examples

---
+++ JavaScript/Conceitos/Dados/tiposDeDados.js
@@ -0,0 +1,7 @@
+var nome = 'Lucas'; //String
+var numero = 3; //Numero
+var ok = true //Boolean
+var time; //Undefined
+var comida = null; //Nulo
+var simbolo = Symbol() //Simbolo
+var objt = {} //Objeto
