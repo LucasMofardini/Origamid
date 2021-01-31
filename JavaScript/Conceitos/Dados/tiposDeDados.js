b324ab7138322c72cf1e4b6b75b19846d4f7ad74
Lucas Mofardini <lucasmofardini@gmail.com>
2021-01-30 23:54:54 -0300

    [JS] Type of data - examples

--- JavaScript/Conceitos/Dados/tiposDeDados.js
+++ JavaScript/Conceitos/Dados/tiposDeDados.js
@@ -173,3 +173,52 @@ switch (corFavorita) {
     default:
         console.log('feche os olhos');
 }
+// Exercicio
+
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var idadeParente = 50;
+if (idade > idadeParente) {
+    console.log('É maior');
+} else if (idade === idadeParente) {
+    console.log('É igual');
+} else {
+    console.log('É menor');
+}
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+// 3
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nomeA = 'Andre';
+var idadeA = 28;
+var possuiDoutoradoA = false;
+var empregoFuturoA;
+var dinheiroNaContaA = 0;
+console.log(!!nomeA);
+console.log(!!idadeA);
+console.log(!!possuiDoutoradoA);
+console.log(!!empregoFuturoA);
+console.log(!!dinheiroNaContaA);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+console.log('Brasil e china');
+console.log(brasil >= china);
+// O que irá aparecer no console?
+if (('Gato' === 'gato') && (5 > 2)) {
+    //false               //verdadeiro
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
+//Vai aparecer Falso
+// O que irá aparecer no console?
+if (('Gato' === 'gato') || (5 > 2)) {
+    //false               //verdadeiro
+
+    console.log('Gato' && 'Cão');
+} else {
+    console.log('Falso');
+}
+//Vai aparecer 'cao'
