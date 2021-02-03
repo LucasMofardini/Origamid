ea082b314028886f1930b45bdf495842b362b400
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-02 22:32:09 -0300

    [JS] Object - 2 training

--- JavaScript/Conceitos/Objeto/script.js
+++ JavaScript/Conceitos/Objeto/script.js
@@ -45,3 +45,38 @@ console.log(menu.background); // black
 menu.color = 'white';
 console.log(menu) // Agora tem color white
 console.log(menu.metadeHeight());
+
+//O objeto menu tem herança do Object
+
+//-------------- Exercicio --------------
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var person = {
+        nome: 'Lucas',
+        sobrenome: 'Mofardini',
+        idade: 19,
+    }
+    // Crie um método no objeto anterior, que mostre o seu nome completo
+person.mostrar = function() {
+    return `${this.nome} ${this.sobrenome}`;
+}
+console.log(person);
+console.log(person.mostrar());
+
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+}
+carro.preco = 3000;
+console.log(carro.preco);
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorro = {
+    raça: 'Labrador',
+    cor: 'Preto',
+    idade: 10,
+    Late: true,
+}
+console.log(cachorro)
