95210fbaef9af827f5e28b18c2da8c69523a562a
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-07 00:39:57 -0300

    [JS]  DOM for beginners

--- JavaScript/Conceitos/DOM/script.js
+++ JavaScript/Conceitos/DOM/script.js
@@ -1,1 +1,18 @@
 console.log('DOM');
+// DOM PARA INICIANTES
+
+//Document object model
+//Manipulação de estrutura, de css, conteudo.
+
+// window = todo dom 
+console.log(window.innerHeight); //altura do site
+// window = objeto
+console.log(window.location.href);
+
+//Estrutura de ARVORE
+const titulo = document.querySelector('h1');
+titulo.addEventListener('click', function() {
+    alert('Click');
+});
+
+console.log(document.body);
