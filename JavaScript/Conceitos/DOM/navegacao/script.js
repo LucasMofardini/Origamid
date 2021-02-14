bed15c2d7ac3ebf038354c9b4661389c552592a2
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-14 05:01:32 -0300

    [JS] Navigation by tabs - finish

--- JavaScript/Conceitos/DOM/navegacao/script.js
+++ JavaScript/Conceitos/DOM/navegacao/script.js
@@ -1,1 +1,22 @@
 //Navegação por tab
+//Encontrando os elementos
+const tabmenu = document.querySelectorAll('.js-tabmenu li');
+const tabcontent = document.querySelectorAll('.js-tabcontent section');
+if (tabmenu.length && tabcontent.length) {
+  tabcontent[0].classList.add('ativo');
+
+  function activeTab(index) {
+    //Remove todos
+    tabcontent.forEach((section) => {
+      section.classList.remove('ativo');
+    });
+    //Adiciona ativo no index correspondente
+    tabcontent[index].classList.add('ativo');
+  }
+
+  tabmenu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
+}
