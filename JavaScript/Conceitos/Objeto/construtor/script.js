3780b24bef10c3f292d5c1da0646460a3786d19b
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-16 02:55:07 -0300

    [JS] Constructor function - real

--- JavaScript/Conceitos/Objeto/construtor/script.js
+++ JavaScript/Conceitos/Objeto/construtor/script.js
@@ -3,6 +3,7 @@ function Carro(marca, preco) {
     this.marca = marca;
     this.preco = preco;
 }
+
 const honda = new Carro('honda', 3000); /* honda é um objeto do construtor carro */
 const fiat = new Carro('fiat', 2000);
 
@@ -25,3 +26,43 @@ return honda = {
     preco: 0,
 } 
 */
+
+function Carro2(marca, preco) {
+    const taxa = 1.2; // Ela nao é exposta no código
+    const precoF = preco * taxa; // Ela nao é exposta no código
+    this.marca = marca;
+    this.preco = precoF;
+}
+const mazda = new Carro2('mazda', 5000); // precofinal sai 6000
+
+//Real
+/* const Dom = {
+    seletor: 'li',
+    element() {
+        const elementoSelecionado = document.querySelector(this.seletor);
+        return elementoSelecionado;
+    },
+    ativar() {
+        this.element().classList.add('ativo');
+    }
+}
+Dom.ativar(); // Ativa a Li
+//E se quiser ativar a Ul ? 
+Dom.seletor = 'ul';
+Dom.ativar(); // Ativa a UL */
+
+//Transformando em função construtora
+
+function Dom(seletor) {
+    this.element = () => {
+        return document.querySelector(seletor);
+    }
+    this.ativar = (classe) => {
+        this.element().classList.add(classe);
+    }
+}
+const li = new Dom('li'); //Objeto li
+const ul = new Dom('ul'); //Objeto ul 
+//Ambos com as mesmas propriedades
+li.ativar('ativo'); // Primeiro Li da página
+ul.ativar('Lucas'); // Primeiro Ul da página
