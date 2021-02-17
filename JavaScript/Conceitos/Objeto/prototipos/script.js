217a7fd4af8b2647a8daf39fcb752cb6c5f88687
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-17 10:35:21 -0300

    [JS] Prototype - training

--- JavaScript/Conceitos/Objeto/prototipos/script.js
+++ JavaScript/Conceitos/Objeto/prototipos/script.js
@@ -50,3 +50,48 @@ console.log(String.prototype);
 console.log(carro.andar()); // Retorna Boleano, True or False
 // carro.andar.constructor.name = Function;
 console.log(carro.andar); // Retorna a função e seus prototypes
+
+console.log('---------- Exercicio ----------');
+
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoas(nome, sobrenome, idade) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+  this.idade = idade;
+}
+Pessoas.prototype.nomeCompleto = function () {
+  return `${this.nome} ${this.sobrenome}`;
+};
+const p1 = new Pessoas('lucas', 'mofardini', 19);
+
+// Liste os métodos acessados por
+// dados criados com NodeList,
+// HTMLCollection, Document
+console.log(NodeList.prototype);
+console.log(HTMLCollection.prototype);
+console.log(Document.prototype);
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li;
+console.log(li.constructor.name); // Elemento Html
+li.click;
+console.log(li.click.constructor.name); // Function
+li.innerText;
+console.log(li.innerText.constructor.name); // String
+li.value;
+console.log(li.value.constructor.name); // Number
+li.hidden;
+console.log(li.hidden.constructor.name); // Boolean
+li.offsetLeft;
+console.log(li.offsetLeft.constructor.name); // Number
+li.click();
+console.log(li.click.constructor.name); // Function
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // Boolean
+console.log('Construtor do li: ' + li.hidden.constructor.name);
