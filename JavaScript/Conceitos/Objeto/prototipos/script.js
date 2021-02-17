e8d8c9e0beda4a269f8045bebbf8ce2de655bdda
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-17 10:21:11 -0300

    [JS] Prototype - 2

--- JavaScript/Conceitos/Objeto/prototipos/script.js
+++ JavaScript/Conceitos/Objeto/prototipos/script.js
@@ -16,5 +16,37 @@ console.log(lucas.prototype); // undefined
 console.log(Pessoa.prototype); // retorna o objeto
 
 // __proto__
+
 // Tem em todo construtor
 // Mas não usar nele direto
+const pais = 'Brasil';
+const cidade = new String('Rio');
+
+pais.charAt(0); // B
+cidade.charAt(0); // R
+
+const listaAnimais = ['Cachorro', 'Gato', 'Cavalo'];
+
+const lista = document.querySelectorAll('li'); // Node List
+//Protoype de NodeList tem funçoes de NodeList
+//São diferentes mas parecem iguais
+
+// Transforma em uma array
+const listaArray1 = Array.prototype.slice.call(lista); //Array
+const listaArray2 = Array.from(lista); //Array
+//Protoype de array tem funçoes de array
+
+const carro = {
+  marca: 'Ford',
+  preco: 2000,
+  andar() {
+    return true;
+  },
+};
+console.log(Number.prototype);
+console.log(String.prototype);
+
+// carro.andar().constructor.name = Boolean;
+console.log(carro.andar()); // Retorna Boleano, True or False
+// carro.andar.constructor.name = Function;
+console.log(carro.andar); // Retorna a função e seus prototypes
