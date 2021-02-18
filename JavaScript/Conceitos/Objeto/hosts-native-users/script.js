b6b44b52555d800a7c9fddb7ac11709ea59a0b41
Lucas Mofardini <lucasmofardini@gmail.com>
2021-02-18 08:38:07 -0300

    [JS] Object hosts, native and users

---
+++ JavaScript/Conceitos/Objeto/hosts-native-users/script.js
@@ -0,0 +1,33 @@
+//Nativos
+Object;
+String;
+Array;
+Function;
+
+//Hosts
+NodeList;
+DOM;
+Element;
+
+//Users
+// const Pessoa = {
+//     nome: 'André';
+//   }
+
+//ECMA Define padroes de tecnologia
+//ECMA Script 2015 trouxe muita coisa nova
+
+// DIFERENTES VERSÕES
+// Browsers diferentes
+// Versões de browsers
+
+//Verificar se existe o ECMA
+console.log(typeof Array.from !== undefined);
+if (typeof Array.from !== undefined) {
+  console.log('Existe');
+} else {
+  console.log('Não existe');
+}
+
+//API
+// Application Programming Interface
