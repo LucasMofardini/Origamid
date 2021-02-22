af530c7cb8c53c9941ce3b5221bd0755fc7043bd
lucasmofardini <lucasmofardini@gmail.com>
2021-02-21 21:53:38 -0300

    [JS] Array

---
+++ JavaScript/Conceitos/Objeto/array/script.js
@@ -0,0 +1,12 @@
+// Armazena colecao de elementos
+//String numero funçã array
+
+const instrumentos = ['Guitarra', 'Baixo', 'Violão'];
+const carros = new Array('Corola', 'Mustang', 'Honda');
+
+carros[1] // Mustang
+carros[2] = 'Ferrari';
+carros[10] = 'Parati';
+carros.length; // 11 //Tamanho da array
+
+const li = document.querySelectorAll('li');
