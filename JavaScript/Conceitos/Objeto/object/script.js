d2287bbc7c2c602be50dfdafa2f5526415d6a3ae
Lucas Mofardini <lucasmofardini@gmail.com>
2021-03-02 22:21:35 -0300

    [JS] Object - 2

--- JavaScript/Conceitos/Objeto/object/script.js
+++ JavaScript/Conceitos/Objeto/object/script.js
@@ -35,31 +35,100 @@ const funcaoAutomovel = {
     },
 }
 
-const moto = {
 
-}
-
-const carro = {
-    rodas: 4,
-    mala: true,
-};
+const moto = {};
 //Adiciona funçao automovel ao objeto moto
 //Entra dentro dela direto
 //Se a propriedade existir ele coloca em cima
-Object.assign(moto, funcaoAutomovel);
-Object.assign(carro, funcaoAutomovel);
+// Object.assign(moto, funcaoAutomovel);
+// Object.assign(carro, funcaoAutomovel);
 
 Object.defineProperties(moto, {
     rodas: {
         // value: 2, //Valor
         //Nao pode ter valor quando tem get e set
         configurable: false, //Nao é modificavel
-        writable: false, //[Por padrao é false] mudar o valor 
-        enumerable: false, //[Por padrao é false] torna numeravel
+        // writable: false, //[Por padrao é false] mudar o valor 
+        // enumerable: false, //[Por padrao é false] torna numeravel
         get() {
-            return 2;
+
+            return this._rodas;
             //Toda vez que pedir rodas da moto, retorna 2
+
+        },
+        set(valor) {
+            return this._rodas = valor * 2;
         },
     },
-    capacete: true,
+    capacete: {
+        value: true,
+    }
 });
+
+// moto.rodas = 2; // Setei a quantidade de rodas
+// moto.rodas; // O get dela sai 4 pois multipliquei por 2
+
+//Lista as propriedades de um objeto
+Object.getOwnPropertyDescriptors(Array);
+// Lista com métodos e propriedades e Array
+
+Object.getOwnPropertyDescriptors(Array.prototype);
+// Lista com métodos e propriedades do protótipo de Array
+
+Object.getOwnPropertyDescriptor(window, 'innerHeight');
+// Puxa de uma única propriedade
+
+
+Object.keys(Array);
+// [] vazia, pois não possui propriedades enumeráveis
+
+const carro1 = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.keys(carro1);
+// ['marca', 'ano']]
+//propriedade
+Object.values(carro1);
+// ['Ford', 2018]
+//Valor
+Object.entries(carro1);
+// [['marca', 'Ford'], ['ano', 2018]]
+//propriedade valor
+
+Object.getOwnPropertyNames(Array);
+// ['length', 'name', 'prototype', 'isArray', 'from', 'of']
+
+Object.getOwnPropertyNames(Array.prototype);
+// [..., 'filter', 'map', 'every', 'some', 'reduce', ...]
+//Mostra em string
+const carro2 = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.getOwnPropertyNames(carro2);
+// ['marca', 'ano']
+
+const frutas = ['Banana', 'Pêra'];
+//Prototipo de frutas
+Object.getPrototypeOf(frutas);
+//Prototipo do objeto String
+Object.getPrototypeOf(''); // String
+
+const frutas1 = ['Banana', 'Pêra'];
+const frutas2 = ['Banana', 'Pêra'];
+
+Object.is(frutas1, frutas2); // false
+//Os 2 objetos nao são iguais
+
+const car = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.freeze(car); // Freeza o objeto car, nao deixa fazer nada
+Object.seal(car); // Nao deixa criar novas propriedades mas deixa mudar
+Object.preventExtensions(car); // igual o seal mas deixa deletar as propriedades
+
+Object.isFrozen(car); // true
+Object.isSealed(car); // true
+Object.isExtensible(car); // true
