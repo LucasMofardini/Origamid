81183d39d2c17d3ca2a34f8eb40a89573961121e
Lucas Mofardini <lucasmofardini@gmail.com>
2021-03-04 23:49:03 -0300

    [JS] dataset - 2

--- JavaScript/Conceitos/DOM/efeitos-dom/dataset/script.js
+++ JavaScript/Conceitos/DOM/efeitos-dom/dataset/script.js
@@ -1,11 +1,13 @@
-const div = document.querySelector('[data-cor]');
-console.log(div.dataset);
-div.dataset.height = 1000;
-delete div.dataset.width;
+// Adicione um atributo data-anime="show-down" e
+// data-anime="show-right" a todos as section's
+// com descricão dos animais.
 
-// {animaScroll: 'left'}
-div.dataset.animaScroll; // left
-div.dataset.tempoTotal = 1000;
-// Na div vira data-tempo-total="1000"
+// Utilizando estes atributos, adicione a classe
+// show-down ou show-right a sua respectiva section
+// assim que a mesma aparecer na tela (animacao tab)
 
-delete div.dataset.animaScroll; // remove o atributo
+// No CSS faça com que show-down anime de cima para baixo
+// e show-right continue com a mesma animação da esquerda
+// para a direita
+
+// Substitua todas as classes js- por data atributes.
