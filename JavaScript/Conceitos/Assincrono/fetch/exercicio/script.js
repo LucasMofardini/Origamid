6ccbdabac72ea9411ecb6230a6c035eab10e1a23
Lucas Mofardini <lucasmofardini@gmail.com>
2022-03-28 20:58:07 -0300

    [JAVASCRIPT] pt 1 - exercicio

---
+++ JavaScript/Conceitos/Assincrono/fetch/exercicio/script.js
@@ -0,0 +1,34 @@
+
+
+const formCep = document.querySelector('#form-cep');
+const btnBuscar = document.querySelector('#btn-buscar');
+const inputCep = document.querySelector('#cep');
+
+const handleCep = (json) => {
+    formCep.logradouro.value = json.logradouro;
+    formCep.localidade.value = json.localidade;
+    formCep.uf.value = json.uf;
+
+}
+function buscaCep() {
+    let cepNumero = inputCep.value;
+
+    if (cepNumero) {
+        let cep = fetch(`https://viacep.com.br/ws/${cepNumero}/json/`);
+
+        cep.then((response) => {
+            return response.json();
+        }).then((body) => {
+            handleCep(body);
+        }).catch((error) => {
+            console.error(error);
+        });
+    } else {
+        console.error('Cep Vazio');
+    }
+
+
+}
+
+btnBuscar.addEventListener('click', buscaCep);
+inputCep.addEventListener('change', buscaCep);
--- JavaScript/Conceitos/Assincrono/fetch/script.js
+++ JavaScript/Conceitos/Assincrono/fetch/script.js
@@ -8,8 +8,9 @@ doc.then((response) => {
     // Por isso se usa o then
     // const conteudo = document.querySelector('.conteudo');
     // conteudo.innerText = body;
-    // console.log(body);
+    console.log(body);
 });
+
 
 // ------------ // 
 
@@ -19,9 +20,10 @@ cep.then((response) => {
     return response.json();
 }).then((body) => {
     const conteudo = document.querySelector('.conteudo');
-    // console.log(body);
+    console.log(body);
     conteudo.innerText = body.logradouro;
 });
+
 
 // Quando vai utilizar o text() ?
 // Quando voce quer o texto de algo, html, o corpo de um arquivo css e/ou javascript
@@ -29,6 +31,7 @@ cep.then((response) => {
 // Vou 'injetar' o arquivo css via fetch : 
 
 const css = fetch('./style.css');
+
 
 css.then((response) => {
     return response.text();
@@ -42,6 +45,7 @@ css.then((response) => {
     conteudo.appendChild(style);
 })
 
+
 // Vou 'injetar' o html via fetch
 
 const html = fetch('./sobre.html');
@@ -53,10 +57,11 @@ html.then((response) => {
 }).then((respostaTexto) => {
     div.innerHTML = respostaTexto;
     const tituloHtml = div.querySelector('h1');
-    // console.log(tituloHtml)
+    console.log(tituloHtml)
     const h1Documento = document.querySelector('h1');
     h1Documento.innerHTML = tituloHtml.innerText;
 })
+
 
 // Outro formato é o blob
 // Ele representa um arquivo, imagem ...
@@ -83,10 +88,10 @@ const cepClone = fetch('https://viacep.com.br/ws/05885620/json/');
 cepClone.then((response) => {
     const response2 = response.clone();
     response.text().then((rText) => {
-        // console.log(rText);
+        console.log(rText);
     })
     response2.json().then(rJson => {
-        // console.log(rJson);
+        console.log(rJson);
 
     });
 
@@ -95,23 +100,29 @@ cepClone.then((response) => {
 
 // Propriedade Headers da resposta 
 // Cabeça da requisição
+
 const cepHeader = fetch('https://viacep.com.br/ws/05885620/json/');
 
 cepHeader.then((response) => {
-    // console.log(response);
+
+    console.log(response);
+
     response.headers.forEach((item) => {
-        // console.log(item);
+        console.log(item);
 
     });
+
 });
 
 // status : retorna o status da requisião
 // url : retorna a url da requisiçao
 // type : é o tipo da requisiçao, se é pro proprio servidor etc
+
 const cepStatus = fetch('https://viacep.com.br/ws/05885620/json/');
 
 cepStatus.then((response) => {
     console.log(response.status);
+
     if (response.status === 200) {
         console.log(response.url);
         console.log(response.type);
@@ -119,3 +130,4 @@ cepStatus.then((response) => {
     }
 
 });
+
