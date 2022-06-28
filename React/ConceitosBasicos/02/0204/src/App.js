3617db6b6b12481197cfb212b47486cd4f91fd4c
Lucas Mofardini <lucasmofardini@gmail.com>
2022-06-28 16:33:45 -0300

    [React] 0201 a 0204

---
+++ React/ConceitosBasicos/02/0204/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+const App = () =>{
+    return(
+        // React.createElement('h1', null , 'APP')
+        <h1> Ola Mundo </h1>
+    )
+}
+
+export default App;
