3040591ef69eb34646efab86ba6dbbf4ab9994bd
Lucas Mofardini <lucasmofardini@gmail.com>
2022-06-29 18:05:50 -0300

    [React] 207 exercicio modify

--- React/ConceitosBasicos/02/meuapp/src/Exercicio207.js
+++ React/ConceitosBasicos/02/meuapp/src/Exercicio207.js
@@ -5,50 +5,53 @@ import React from "react";
 // Se a situação estiver ativa pinte de verde, inativa vermelho
 // Se o gasto for maior que 10000 mostre uma mensagem
 const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
-  
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-  };
-  let valorSomado = 0;
-  const valorMaior = 10000;
-  const Exercicio207 = () => {
-    const dados = luana;
-  
-    return(
-        <div>
-            <p>Nome: {dados.cliente}</p>
-            <p>Idade: {dados.idade}</p>
-            <p>Situação: {!dados.ativa ? 
-                <span style={{color: 'red'}}> Inativa </span> : 
-                <span style={{color: 'green'}}> Ativa </span> }
-            </p>
-            <p>Total gasto: R$ 
-                { valorSomado = dados.compras
-                   .reduce((soma, item)=>{
-                        const resultado = soma + (+item.preco.replace('R$', ''));
-                        return resultado;
-                    },0)}
-            </p>
-            {valorSomado > valorMaior && <p>Voce está gastanto muito</p>}
-        </div>
-    );
-  };
+  cliente: "Luana",
+  idade: 27,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+  ],
+  ativa: true,
+};
 
-export default Exercicio207
+const mario = {
+  cliente: "Mario",
+  idade: 31,
+  compras: [
+    { nome: "Notebook", preco: "R$ 2500" },
+    { nome: "Geladeira", preco: "R$ 3000" },
+    { nome: "Smartphone", preco: "R$ 1500" },
+    { nome: "Guitarra", preco: "R$ 3500" },
+  ],
+  ativa: false,
+};
+
+const Exercicio207 = () => {
+  const dados = luana;
+  const valorMaior = 10000;
+
+  const total = dados.compras.reduce((soma, item) => {
+    const resultado = soma + +item.preco.replace("R$", "");
+    return resultado;
+  }, 0);
+
+  return (
+    <div>
+      <p>Nome: {dados.cliente}</p>
+      <p>Idade: {dados.idade}</p>
+      <p>
+        Situação:{" "}
+        {!dados.ativa ? (
+          <span style={{ color: "red" }}> Inativa </span>
+        ) : (
+          <span style={{ color: "green" }}> Ativa </span>
+        )}
+      </p>
+      <p>Total: R$ {total}</p>
+      {total > valorMaior && <p>Voce está gastanto muito</p>}
+    </div>
+  );
+};
+
+export default Exercicio207;
