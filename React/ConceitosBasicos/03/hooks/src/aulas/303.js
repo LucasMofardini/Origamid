425eedbecdb1c7a7004fb983ea5a006b23d0fac8
Lucas Mofardini <lucasmofardini@gmail.com>
2022-07-20 22:41:59 -0300

    [React] 303 exempla

--- React/ConceitosBasicos/03/hooks/src/aulas/303.js
+++ React/ConceitosBasicos/03/hooks/src/aulas/303.js
@@ -2,19 +2,28 @@ import React, { useRef, useState }  from 'react';
 
 function Aula303 (){
     const [comentarios, setComentarios] = useState([]);
-    const [input, setInput]= useState('');
+    const [input, setInput] = useState('');
+    const [notificacao, setNotificacao] = useState(null);
+
     const inputElement = useRef();
+    const timeoutRef = useRef();
 
     function handleClick(){
         setComentarios((prevState)=>{
             return [...prevState, input];
         })
         setInput('');
+        setNotificacao('Foi colocado na lista');
         inputElement.current.focus();
+        clearInterval(timeoutRef.current)
+        timeoutRef.current = setTimeout(()=>{
+          setNotificacao(null)
+        }, 1000)
     }
 
     return (
         <>
+            <p>{notificacao}</p>
             <ul>
                 {comentarios.map((item)=><li key={item}>{item}</li>)}
             </ul>
